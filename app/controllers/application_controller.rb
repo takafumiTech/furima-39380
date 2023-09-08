0a9a9c3eb9da9e66786e7f7c2b6f9ef54a658eee
takafumiTech <t.k.basket.f@gmail.com>
2023-09-08 09:07:39 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname],[:first_name_kanji],[:last_name_kanji],[:first_name_furigana],[:last_name_furigana],[:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname][:first_name_kanji][:last_name_kanji][:first_name_furigana][:last_name_furigana][:birthday])
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presence: true
+  validates :first_name_kanji,    presence: true
+  validates :last_name_kanji,     presence: true
+  validates :first_name_furigana, presence: true
+  validates :last_name_furigana,  presence: true
+  validates :birthday,            presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角文字を使用してください" } do
+    validate :first_name_kanji
+    validate :last_name_kanji
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "英数字混在で入力してください" } do
+    validate :encrypted_password
+
+  with_options presence: true, format: { with: ,[ァ-ヶ一] message: "全角カナを使用してください" } do
+    validates :first_name_furigana
+    validates :last_name_furigana
+
+  has_many :items
+  has_many :comments
+  has_many :orders
 end
