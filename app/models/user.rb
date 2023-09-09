e4c8788f6d7a0191c9ce9886e66c0ed8b1ceb46f
takafumiTech <t.k.basket.f@gmail.com>
2023-09-09 16:39:11 +0900

    rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   validates :nickname,            presence: true
   validates :birthday,            presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角文字を使用してください" } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name_kanji
     validates :last_name_kanji
   end
 
-  with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "英数字混在で入力してください" } do
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英数字混在で入力してください' } do
     validates :password
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
     validates :first_name_furigana
     validates :last_name_furigana
   end
