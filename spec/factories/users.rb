c58ed1012609c9d809a6731fb3b1db6d6d4e17e5
takafumiTech <t.k.basket.f@gmail.com>
2023-09-09 16:32:56 +0900

    gem'gimei’の導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :user do
+    transient do 
+      person { Gimei.name }
+    end
+
     nickname              {Faker::Name.initials(number:2)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length:6)}
     password_confirmation {password}
-    first_name_kanji      {"佐藤"}
-    last_name_kanji       {"太郎"}
-    first_name_furigana   {"サトウ"}
-    last_name_furigana    {"タロウ"}
-    birthday              {"2000-01-01"}
+    first_name_kanji      {person.first.kanji}
+    last_name_kanji       {person.last.kanji}
+    first_name_furigana   {person.first.katakana}
+    last_name_furigana    {person.last.katakana}
+    birthday              {Faker::Date.backward}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
-      binding.pry
       expect(@user.errors.full_messages).to include ("Nickname can't be blank")
     end
 
