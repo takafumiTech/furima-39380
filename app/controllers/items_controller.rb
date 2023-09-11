b169ddd4a69f653fb423891f8ab1548c894a53c3
takafumiTech <t.k.basket.f@gmail.com>
2023-09-11 22:55:06 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,9 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :quality_id, :delivery_fee_id, :prefecture_id, :processing_time_id).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  belongs_to       :user
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :quality
