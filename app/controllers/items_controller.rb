8e347fe27e672e209511837ac657db5e1e78849f
takafumiTech <t.k.basket.f@gmail.com>
2023-09-29 17:00:19 +0900

    購入済の変更不可の仕様追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id && @item.order.present?
       redirect_to root_path
     end
   end
